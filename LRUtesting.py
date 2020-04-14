1e9c802a0ad5b714c097a010e9f75f256384f1be
swapnith8 <kswapnith@msitprogram.net>
2020-04-15 02:43:12 +0530

    Assertion error solved and all test cases passed

--- LRUtesting.py
+++ LRUtesting.py
@@ -17,12 +17,15 @@ def main():
         Test_obj.put(1)
         Test_obj.put(2)
         Test_obj.put(3)
-        Test_obj.put(4)
+        Test_obj.put(2)
+        assert True == Test_obj.get(1)
+        assert True == Test_obj.get(2)
+        assert [1,3,2] == Test_obj.get_cache()
         Test_obj.put(4)
         Test_obj.put(5)
+        Test_obj.put(4)
         Test_obj.put(6)
-        assert True == Test_obj.get(1)
-        assert [1,2,3,5,6] == Test_obj.get_cache()
+        assert [1,3,2,5,4,6] == Test_obj.get_cache()
         print("All the Test cases are passed")
 
 if __name__ == '__main__':
