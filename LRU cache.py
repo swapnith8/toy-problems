8a99593a440fb5d1b291f673837987b142b42f96
swapnith8 <kswapnith@msitprogram.net>
2020-04-15 00:57:16 +0530

    add constructor for LRU

--- LRU cache.py
+++ LRU cache.py
@@ -4,9 +4,4 @@ class LRUCache():
     def _init_(self):
         self.list=[]
 
-    def put(self,k):
-        pass
-    def get(self,k): 
-        pass
-    def get_cache(self,k):
-        pass
+    
