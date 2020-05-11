9c4780380ac4df8428a52f60f2bef650001ed870
swapnith8 <kswapnith@msitprogram.net>
2020-05-11 15:07:57 +0530

    Done with addig tasks

---
+++ TODO/todo.js
@@ -0,0 +1,32 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var s = document.createElement("S");
+  var cont = document.createTextNode("");
+  s.className = "close";
+  s.appendChild(cont);
+  myNodelist[i].appendChild(s);
+}
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("Task").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+  alert("please enter text!!");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("Task").value = "";
+  var s = document.createElement("S");
+  var cont = document.createTextNode("");
+  s.className = "close";
+  s.appendChild(cont);
+  li.appendChild(s);
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+  }
+}
